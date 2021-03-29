2ac9eb966d143dcd1be42deb4cf66abf1896ba49
Arsh Aulakh <arsh@upgame.co>
2021-03-29 11:36:53 +0530

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMapsUtils",
-            url: "https://github.com/ArshAulakh59/google-maps-ios-utils/releases/download/v3.8.4/GoogleMapsUtils.xcframework.zip",
-            checksum: "808bb7c9dfa5804629f40294aa9ba5ca172bad09edf1e3851dc3b18e0fa9816a"
-        )
+            url: "https://github.com/ArshAulakh59/google-maps-ios-utils/releases/download/v3.8.5/GoogleMapsUtils.xcframework.zip",
+            checksum: "f8935badb1563611d5342a38c8a5fae61287aa4097e7f5f57f744c9d909ce8e2"
+        ),
     ]
 )
