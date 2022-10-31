79b0976b1c6b574cc4f6c732b1adda53503f1754
ArshAulakh <arshaulakhios@gmail.com>
2022-10-31 09:32:48 +0530

    Update AppDelegate.swift

--- samples/SwiftDemoApp/SwiftDemoApp/AppDelegate.swift
+++ samples/SwiftDemoApp/SwiftDemoApp/AppDelegate.swift
@@ -17,7 +17,7 @@ import GoogleMaps
 import UIKit
 
 // Change this key to a valid key registered with the demo app bundle id.
-let mapsAPIKey = "AIzaSyBG1nvcnNgfR3eEVduXjEHQ3eGs4quNqds"
+let mapsAPIKey = ""
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
